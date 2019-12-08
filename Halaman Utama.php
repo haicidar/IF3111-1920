3e1e35623fad6fec7eb2051572dced4d18bae00e
usagifm <44514120+usagifm@users.noreply.github.com>
2019-12-09 03:27:29 +0700

    Commit pertama untuk Halaman utama dan cssnya

---
+++ Halaman Utama.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+
+
+    <title>
+
+        Laporan geh !
+
+    </title>
+
+    <link href="Style-HalamanUtama.css" rel="stylesheet" type="text/css">
+
+</head>
+
+
+<body>
+
+        <div class="banner">Simple lapor !</div>
+
+        <div class="atastextarea" text-align="center">
+            
+        <a href="" >Buat Laporan/Komentar</a>
+
+            </div>
+    
+        <form method="POST" action="#######" accept-charset="UTF-8" data-request="complaint::onCreate" class="complaint-form" data-request-flash=""><input name="_session_key" type="hidden" >
+
+
+   <div class="container-1">
+      
+      <input type="search" id="search" placeholder="Search..." />
+
+
+
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br><br><br><br><br><br>
+<hr width="70%">
+
+</form>
+
+</div>
+
+</body>
+</html>

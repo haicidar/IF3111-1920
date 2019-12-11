82c552cd7e992d2f2439afb4e6ae724fb4af5726
usagifm <44514120+usagifm@users.noreply.github.com>
2019-12-12 01:09:13 +0700

    added more feature on Lapor and Home page , (rev2)

--- Halaman Utama.php
+++ Halaman Utama.php
@@ -9,10 +9,22 @@
     </title>
 
     <link href="Style-HalamanUtama.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 
-
+<div id="navbar">
+        <a href="javascript:void(0)">LAPORAN</a>
+        <a class="active" href="javascript:void(0)">BERANDA</a>
+        <div class="search-container">
+                <form action="/action_page.php">
+                  <input type="text" placeholder="Search.." name="search">
+                  <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
+              </div>
+      </div>
 <body>
 
         <div class="banner">Simple lapor !</div>
@@ -45,6 +57,38 @@
 </form>
 
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </body>
 </html>

680d7c9a4eb4d70f477366d4193379bc263e7a26
usagifm <44514120+usagifm@users.noreply.github.com>
2019-12-14 17:52:39 +0700

    Added validation feature !

--- create.blade.php
+++ create.blade.php
@@ -8,10 +8,14 @@
 
     </title>
 
-    <link rel="stylesheet" type="text/css" href="Style-HalamanLapor.css">
+    <link rel="stylesheet" type="text/css" href="laporStyle.css">
     <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+    
+
 
 </head>
 
@@ -28,7 +32,7 @@
 
         <div class="atastextarea">Buat Laporan/Komentar</div>
     <div class="lapor">
-        <form method="POST" action="#######" accept-charset="UTF-8" data-request="complaint::onCreate" class="complaint-form" data-request-flash=""><input name="_session_key" type="hidden" >
+        <form method="POST"  action="#######" onSubmit="return validasi()" id="formtambah" >
 
 
   
@@ -38,16 +42,20 @@
 
             
             <input type="text" name="judul"  placeholder="Masukan judul laporan anda...">  
-<textarea name="isi-laporan" id="" cols="83" rows="10" class="form-control textarea-flex autosize" placeholder="Ketik laporan Anda..."></textarea>
+<textarea name="isi-laporan"  cols="83" rows="10" class="form-control textarea-flex autosize" placeholder="Ketik laporan Anda..."></textarea>
 <div class="hrtextarea" >
 <br>
 </div>
 </div>
 <div class="styled-select">
 
-<select placeholder="Pilih Kategori" value="" >
+<select placeholder="Pilih Kategori" value="" name="aspek">
 <option value="">   --Pilih Aspek Pelaporan/Komentar-- </option>
-<option value="Pelecehan Seksual">Pelecehan Seksual</option>
+<option value="Mahasiswa">Mahasiswa</option>
+<option value="Dosen">Dosen</option>
+<option value="Staff">Staff</option>
+<option value="Pengajaran">Pengajaran</option>
+<option value="Infrastruktur">Infrastruktur</option>
 
 </select>
 
@@ -70,6 +78,13 @@
 <br>
 <br>
 
+
+<?php
+//perintah untuk menjalankan aksi ketika form disubmit disini
+?>
+
+
+
 </form>
 
 
@@ -81,7 +96,72 @@
 
 
 
+<script type="text/javascript">
+// fungsi yang dipanggil ketika form di submit
+// lihat baris 5 onSubmit
+function validasi()
+    {
 
+        var judul=document.forms["formtambah"]["judul"].value;
+        var isi=document.forms["formtambah"]["isi-laporan"].value;
+        var aspek=document.forms["formtambah"]["aspek"].value;
+        
+//        membuat variabel numbers bernilai angka 0 s/d 9
+
+//        validasi nip tidak boleh kosong (required)
+
+        if (judul==null || judul=="")
+          {
+          alert("Judul tidak boleh kosong !");
+          return false;
+          };
+          
+//        validasi nip harus berupa angka
+//        dengan membandingkan dengan variabel number yang dibuat pada baris 21
+
+        if (judul.length >= 100)
+          {
+          alert("Judul harus kurang dari 100 karakter");
+          return false;
+          };
+//         jika ada validasi untuk inputan lain letakkan disini
+//       
+     
+
+    
+//       
+
+     if ( isi==null || isi=="")
+          {
+          alert("Isi laporan tidak boleh kosong !");
+          return false;
+          };
+          
+//        validasi nip harus berupa angka
+//        dengan membandingkan dengan variabel number yang dibuat pada baris 21
+
+        if (isi.length >= 255)
+          {
+          alert("Isi laporan harus kurang dari 255 karakter");
+          return false;
+          };
+//         jika ada validasi untuk inputan lain letakkan disini
+//       
+     
+
+     if (aspek == "" || aspek == null)
+          {
+          alert("Aspek Harus dipilih !");
+          return false;
+          };
+//         jika ada validasi untuk inputan lain letakkan disini
+//       
+     }
+
+
+    
+
+</script>
 
 
 
